1622708644808be7bb3ac7363c2cea597f049acd
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 17:22:27 -0500

    commit3

--- ejercicio28.c
+++ ejercicio28.c
@@ -16,3 +16,14 @@ int main() {
     }
     return 0;
 }
+int es_primo(int n) {
+    if (n < 2) {
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
