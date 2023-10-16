0ff8a72d6946159d4b75ed58e22512539c385a1a
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 17:21:24 -0500

    commit1

---
+++ ejercicio28.c
@@ -0,0 +1,7 @@
+int main() {
+    int num1, num2;
+    printf("Ingrese el primer número entero: ");
+    scanf("%d", &num1);
+    printf("Ingrese el segundo número entero: ");
+    scanf("%d", &num2);
+}
