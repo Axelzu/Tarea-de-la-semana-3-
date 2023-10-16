ae2e4076d4147b6d69f2544ffd220ceafe97221c
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 16:33:19 -0500

    commit4

--- ejercicio1.c
+++ ejercicio1.c
@@ -5,5 +5,11 @@ int main() {
     printf("Ingrese un número entero: ");
     scanf("%d", &numero);
     printf("Los enteros comprendidos entre 1 y %d son:\n", numero);
+     for (i = 1; i <= numero; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
 
 }
