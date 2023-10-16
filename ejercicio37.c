62494b58388395037adb47951c08fb1ffab03450
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 17:38:54 -0500

    commit4

--- ejercicio37.c
+++ ejercicio37.c
@@ -3,8 +3,10 @@ int main (){
     int i,j;
     for (i = 1; i <= 10; i++){
         printf("tabla de multiplicar de  %d\n", i);
-        
+         for (j = 1; j <= 10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
 
     }
-    
+    return 0;
 }
