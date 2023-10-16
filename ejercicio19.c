2e924f176ba5608ac72d24d5e7b15bb676a89538
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 16:49:45 -0500

    commit2

--- ejercicio19.c
+++ ejercicio19.c
@@ -6,5 +6,10 @@ int esPrimo(int numero) {
     if (numero <= 1) {
         return 0;
     }
-   
+   for (i = 2; i * i <= numero; i++) {
+        if (numero % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
