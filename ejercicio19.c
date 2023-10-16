93f08c6a221965c3c8a9ac264b3cb343332bfac3
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-16 16:50:58 -0500

    commit4

--- ejercicio19.c
+++ ejercicio19.c
@@ -17,4 +17,11 @@ int main() {
     int numero;
     printf("Ingrese un número entero: ");
     scanf("%d", &numero);
+    if (esPrimo(numero)) {
+        printf("%d es un número primo.\n", numero);
+    } else {
+        printf("%d no es un número primo.\n", numero);
+    }
+
+    return 0;
 }
