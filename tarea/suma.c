b30c7e25b632489f1b22cd4da775a78e268d424f
Alexander Cahueñas <Alexander Cahueas>
2023-12-03 11:23:13 -0500

    2

--- tarea/suma.c
+++ tarea/suma.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//aqui es para inicializar la matriz con numeros aleatorios entre o y 100
+void inicializarMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    srand(time(NULL));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 101;
+        }
+    }
+}
+// aqui se imprime la matriz 
+void mostrarMatriz(int filas, int columnas, int matriz[filas][columnas]) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
